974b626c1807d40db68140e94f632db745438793
Nisharodge07 <94183743+Nisharodge07@users.noreply.github.com>
2026-06-05 11:22:17 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character,Integer> containsS = new HashMap<Character,Integer>();
+        HashMap<Character,Integer> containsT = new HashMap<Character,Integer>();
+
+        for(int i=0; i<s.length(); i++){
+
+
+            containsS.put(s.charAt(i), containsS.getOrDefault(s.charAt(i),0)+1);
+            containsT.put(t.charAt(i), containsT.getOrDefault(t.charAt(i),0)+1);
+
+
+
+            
+        }
+
+        return containsS.equals(containsT);
+    }
+}
